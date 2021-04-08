7b1e47bf4a4c14a6bbd9c67c4451c6e947c1d326
Craig Worthington <craigworthington33@gmail.com>
2021-04-08 19:02:40 -0400

    changed simulation methodology to allow for easy swapping of routing functions for inspector 1

--- src/models/inspector.py
+++ src/models/inspector.py
@@ -2,7 +2,6 @@ import logging
 import random
 from typing import Generator, List, Union
 
-import simpy
 from simpy import Environment
 
 from .buffer import Buffer
@@ -95,9 +94,62 @@ class Inspector(object):
         self.end = self.env.now
         logging.debug('inspector 2 put component {} in buffer {} at {}'.format(component.name, buffer_list[0].id, self.env.now))
   
+  
+  def add_component_to_buffer_insp_2(self, component: Component) -> Generator:
+    '''
+    Inspector 2's routing policy for adding a component to a buffer
+
+    Args:
+      component: The componetn to be added to a buffer
+    
+    Returns:
+      Generator: the generator used to simulate the placement of the component
+      in the buffer
+    '''
+    # Create list of buffers that will accept a component
+    buffer_list = list(filter(lambda x: (x.will_accept(component)), self.buffers))
+    if len(buffer_list) > 0:
+      self.start = self.env.now
+      yield buffer_list[0].put(amount=1)
+      self.end = self.env.now
+      logging.debug('inspector 2 put component {} in buffer {} at {}'.format(component.name, buffer_list[0].id, self.env.now))
+
+
+
+  def add_component_to_buffer_random(self, component: Component) -> Generator:
+    '''
+    Routing policy for inspector 1 where he places a component in any random buffer
+    that can accept a component.
+    '''
+    # Get a list of all buffers not at capacity
+    available_buffers = list(filter(lambda buf: (buf.level < buf.capacity), self.buffers))
+    
+    # If list is empty
+    if not available_buffers:
+      # Wait for any buffer to become available
+      buffer_ready_events = [buffer.put(amount=1) for buffer in self.buffers]
+      self.start = self.env.now
+      result = yield self.env.any_of(buffer_ready_events)
+      self.end = self.env.now  
+      logging.debug('result of wait for inspector is {}, time={}'.format(result, self.end - self.start))
+    
+    # If there is currently a buffer available
+    else:
+      # select a random buffer from that list
+      buffer_to_use = random.choice(available_buffers)
+
+      # Place the component in that buffer
+      self.start = self.env.now
+      yield buffer_to_use.put(amount=1)
+      self.end = self.env.now
+      
+      
+  
 
   def record_stats(self) -> None:
-    '''Record stats about the inspector'''
+    '''
+    Record stats about the inspector, for later analysis
+    '''
     self.total_amount_inspected[self.current_component.name] += 1
     self.wait_time.append(self.end - self.start) # append buffer wait time
     self.inspection_times[self.current_component.name].append(self.current_inspection_time)
@@ -106,7 +158,16 @@ class Inspector(object):
   def main_loop(self) -> None:
     '''Main sequence loop for Inspector'''
     while True:
+      # Inspect the component
       component = yield self.env.process(self.inspect_component())
-      yield self.env.process(self.add_component_to_buffer(component))
+      
+      # Add the component to a buffer, according to the routing policy
+      # for that inspector/component
+      if (component == Component.C1):
+        yield self.env.process(self.add_component_to_buffer_random(component))
+      else:
+        yield self.env.process(self.add_component_to_buffer(component))
+      
+      # Log stats, if simulation is no longer in the waiting period
       if (self.env.now >= self.start_recording):
         self.record_stats()
